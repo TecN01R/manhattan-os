995d683a86d52fd879dc5b70c68357be2cd2e0c1
Kevin McDole <kpmcdole@gmail.com>
2025-11-17 16:35:22 -0500

    Commit changes

--- modules/desktop-common.nix
+++ modules/desktop-common.nix
@@ -1,5 +1,51 @@
 { config, pkgs, ... }:
 
+let 
+  myWallpaper = pkgs.stdenv.mkDerivation {
+    pname = "custom-wallpaper";
+    version = "1.0";
+    src = pkgs.fetchurl {
+      url = "https://gruvbox-wallpapers.pages.dev/wallpapers/minimalistic/gruvbox_astro.jpg";
+      sha256 = "sha256-YTxyI+vaC5CGQzqMm1enfPh9/1YoqNXAX7TmAscz1U0=";
+    };
+    dontUnpack = true;
+    installPhase = ''
+      mkdir -p $out/share/backgrounds
+      cp $src $out/share/backgrounds/my-wallpaper.jpg
+    '';
+  };
+
+  wallpaperPath = "${myWallpaper}/share/backgrounds/my-wallpaper.jpg";
+
+  gruvboxGtkCustom = pkgs.stdenv.mkDerivation {
+    pname = "gruvbox-gtk-theme-custom";
+    version = "2025-11-15";
+    src = pkgs.fetchFromGitHub {
+      owner = "Fausto-Korpsvart";
+      repo  = "Gruvbox-GTK-Theme";
+      rev   = "578cd220b5ff6e86b078a6111d26bb20ec8c733f";
+      hash  = "sha256-RXoPj/aj9OCTIi8xWatG0QpDAUh102nFOipdSIiqt7o=";
+    };
+    nativeBuildInputs = with pkgs; [
+      sassc
+      gtk-engine-murrine
+      gnome-themes-extra
+    ];
+    installPhase = ''
+      runHook preInstall
+      patchShebangs .
+      mkdir -p $out/share/themes
+      cd themes
+      ./install.sh \
+        -d "$out/share/themes" \
+        --name Gruvbox \
+        --color dark \
+        --size compact \
+        --tweaks medium
+      runHook postInstall
+    '';
+  };
+in
 {
   # Global Nix settings
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
@@ -38,7 +84,9 @@
     gnome-tweaks
     gnome-extension-manager
     resources
-    refine
+
+    gruvboxGtkCustom
+    gruvbox-plus-icons
 
     gnomeExtensions.alphabetical-app-grid
     gnomeExtensions.just-perfection
@@ -46,6 +94,16 @@
     gnomeExtensions.user-themes
     gnomeExtensions.hot-edge
   ];
+
+  environment.etc."gtk-4.0/gtk.css".source =
+    "${gruvboxGtkCustom}/share/themes/Gruvbox-Dark-Compact-Medium/gtk-4.0/gtk.css";
+
+  environment.etc."gtk-4.0/gtk-dark.css".source =
+    "${gruvboxGtkCustom}/share/themes/Gruvbox-Dark-Compact-Medium/gtk-4.0/gtk-dark.css";
+
+  # If the theme has an assets directory:
+  environment.etc."gtk-4.0/assets".source =
+    "${gruvboxGtkCustom}/share/themes/Gruvbox-Dark-Compact-Medium/gtk-4.0/assets";
 
   # Firefox
   programs.firefox = {
@@ -81,6 +139,8 @@
     clock-format='12h'
     clock-show-weekday=true
     show-battery-percentage=true
+    gtk-theme='Gruvbox-Dark-Compact-Medium'
+    icon-theme='Gruvbox-Plus-Dark'
 
     [org.gnome.shell]
     disable-extension-version-validation=true
@@ -113,6 +173,71 @@
 
     [org.gnome.shell.weather]
     automatic-location=true
+
+    [org.gnome.shell.extensions.user-theme]
+    name='Gruvbox-Dark-Compact-Medium'
+
+    ### Blur-My-Shell Extension ###
+
+    [org.gnome.shell.extensions.blur-my-shell]
+    settings-version=2
+
+    [org.gnome.shell.extensions.blur-my-shell.appfolder]
+    brightness=0.6
+    sigma=30
+
+    [org.gnome.shell.extensions.blur-my-shell.coverflow-alt-tab]
+    pipeline='pipeline_default'
+
+    [org.gnome.shell.extensions.blur-my-shell.dash-to-dock]
+    blur=false
+    brightness=0.6
+    pipeline='pipeline_default_rounded'
+    sigma=30
+    static-blur=true
+    style-dash-to-dock=0
+
+    [org.gnome.shell.extensions.blur-my-shell.lockscreen]
+    pipeline='pipeline_default'
+
+    [org.gnome.shell.extensions.blur-my-shell.overview]
+    pipeline='pipeline_default'
+    style-components=2
+
+    [org.gnome.shell.extensions.blur-my-shell.panel]
+    blur=false
+    brightness=0.6
+    pipeline='pipeline_default'
+    sigma=30
+
+    [org.gnome.shell.extensions.blur-my-shell.screenshot]
+    pipeline='pipeline_default'
+
+    [org.gnome.shell.extensions.blur-my-shell.window-list]
+    brightness=0.6
+    sigma=30
+
+    ### Just Perfection Extension ###
+
+    [org.gnome.shell.extensions.just-perfection]
+    accessibility-menu=false
+    animation=5
+    dash-icon-size=48
+    events-button=false
+    keyboard-layout=false
+    panel-button-padding-size=4
+    panel-size=24
+    quick-settings-dark-mode=false
+    ripple-box=false
+    search=false
+    support-notifier-showed-version=34
+    window-preview-caption=false
+    workspace-switcher-size=10
+    world-clock=false
+
+    [org.gnome.desktop.background]
+    picture-uri='file://${wallpaperPath}'
+    picture-uri-dark='file://${wallpaperPath}'
   '';
 
   environment.gnome.excludePackages = with pkgs; [
