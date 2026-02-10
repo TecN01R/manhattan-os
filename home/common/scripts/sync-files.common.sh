d842f8a9425a6d76103ce404f32b324330f323d2
Kevin McDole <kpmcdole@gmail.com>
2026-02-10 08:48:36 -0500

    Wipe

--- home/common/scripts/sync-files.common.sh
+++
@@ -1,4 +0,0 @@
-SYNC_ITEMS=(
-  "sync|$config_home/DankMaterialShell/settings.json|$repo_root/home/common/dms-settings.json|json"
-  "seed-wallpaper|$state_home/DankMaterialShell/session.json|$DMS_WALLPAPER_PATH|json|"
-)
